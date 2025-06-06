a9d402af8767b5e29b71eab11a184f4a1009d507
priyanshugupta1513 <priyanshu35828@gmail.com>
2025-06-06 18:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	let totalCost = 0;
+
+    // Sort the array initially
+    arr.sort((a, b) => a - b);
+
+    while (arr.length > 1) {
+        // Take two smallest
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        // Insert back and sort
+        arr.push(cost);
+        arr.sort((a, b) => a - b); // Keep array sorted
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
